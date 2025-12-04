60d5f8340d42ff06f1f54ddf1f5069e37ee9a14a
cecarlz <calderoncarl1@gmail.com>
2025-12-04 16:12:25 +0800

    Clean up home page imports and remove unused components

--- app/page.tsx
+++ app/page.tsx
@@ -2,11 +2,7 @@ import Hero from '../src/components/widgets/Hero';
 import SocialProof from '../src/components/widgets/SocialProof';
 import Features from '../src/components/widgets/Features';
 import Content from '../src/components/widgets/Content';
-import Steps from '../src/components/widgets/Steps';
 import FAQs2 from '../src/components/widgets/FAQs2';
-// import Pricing from '../src/components/widgets/Pricing';
-import Team from '../src/components/widgets/Team';
-// import CallToAction2 from '../src/components/widgets/CallToAction2';
 import Contact from '../src/components/widgets/Contact';
 
 import { SITE } from '~/config.js';
@@ -23,10 +19,7 @@ import {
   faqs2Home,
   featuresHome,
   heroHome,
-  // pricingHome,
   socialProofHome,
-  stepsHome,
-  teamHome,
 } from '~/shared/data/pages/home.data';
 
 export const metadata: Metadata = {
@@ -46,12 +39,8 @@ export default function Page() {
       <Content {...contentHomeFive} />
       <Content {...contentHomeSix} />
       <Content {...contentHomeSeven} />
-      {/* <Steps {...stepsHome} /> */}
       <FAQs2 {...faqs2Home} />
-      {/* <Pricing {...pricingHome} /> */}
-      {/* <Team {...teamHome} /> */}
       <Contact {...contactHome} />
-      {/* <CallToAction2 {...callToAction2Home} /> */}
     </>
   );
 }
